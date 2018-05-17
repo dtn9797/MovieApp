fa85f19d6382d7aed39d08218a4ee222995645f4
Duy Nguyen <dtn9797@gmail.com>
2018-05-17 00:22:57 -0500

    first commit

--- app/src/main/java/com/example/duynguyen/movieapp/MainActivity.java
+++ app/src/main/java/com/example/duynguyen/movieapp/MainActivity.java
@@ -1,43 +1,79 @@
 package com.example.duynguyen.movieapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.duynguyen.movieapp.Model.APIResponse;
+import com.example.duynguyen.movieapp.Model.Movie;
+
 import java.util.ArrayList;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     ArrayList<Movie> arrayList;
+    String API_BASE_URL = "http://api.themoviedb.org";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         arrayList = new ArrayList<Movie>();
-        arrayList.add(new Movie("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg"));
-        arrayList.add(new Movie("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg"));
-        arrayList.add(new Movie("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg"));
 
-        RecyclerViewApdapter recyclerViewApdapter = new RecyclerViewApdapter(arrayList,this);
-        recyclerView.setAdapter(recyclerViewApdapter);
+        //RETROFIT
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
-        AutoFitGridLayoutManager autoFitGridLayoutManager = new AutoFitGridLayoutManager(this,500);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        Retrofit.Builder builder = new Retrofit.Builder()
+                                        .baseUrl(API_BASE_URL)
+                                        .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit =
+                builder
+                        .client(
+                                httpClient.build()
+                        )
+                        .build();
+        MovieClient client =  retrofit.create(MovieClient.class);
+
+        Call<APIResponse> call = client.top_rated("84d34117b17f3abebe9d04a0325e21c6");
+        call.enqueue(new Callback<APIResponse>() {
+            @Override
+            public void onResponse(Call<APIResponse> call, Response<APIResponse> response) {
+                arrayList = response.body().getResults();
+
+                RecyclerViewApdapter recyclerViewApdapter = new RecyclerViewApdapter(arrayList,MainActivity.this);
+                recyclerView.setAdapter(recyclerViewApdapter);
+
+                AutoFitGridLayoutManager autoFitGridLayoutManager = new AutoFitGridLayoutManager(MainActivity.this,500);
+
+                recyclerView.setLayoutManager(autoFitGridLayoutManager);
+
+            }
+
+            @Override
+            public void onFailure(Call<APIResponse> call, Throwable t) {
+                Log.e("Error","Error in retrofit");
+            }
+        });
+
+
+
+
     }
 
 
---
+++ app/src/main/java/com/example/duynguyen/movieapp/MovieClient.java
@@ -0,0 +1,21 @@
+package com.example.duynguyen.movieapp;
+
+import com.example.duynguyen.movieapp.Model.APIResponse;
+import com.example.duynguyen.movieapp.Model.Movie;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by duynguyen on 5/16/18.
+ */
+
+public interface MovieClient {
+    @GET("/3/movie/popular")
+    Call<APIResponse> popular_movies (@Query("api_key") String apiKey );
+    @GET("/3/movie/top_rated")
+    Call<APIResponse> top_rated (@Query("api_key") String apiKey );
+}
