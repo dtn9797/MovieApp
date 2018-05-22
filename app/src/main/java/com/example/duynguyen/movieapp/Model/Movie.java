66f0605d91e52cbb2718297a942946ecf2f2c81b
Duy Nguyen <dtn9797@gmail.com>
2018-05-22 00:21:00 -0500

    Created intent but need more design. Couldn't load memtrack module ?

--- app/src/main/java/com/example/duynguyen/movieapp/Model/Movie.java
+++ app/src/main/java/com/example/duynguyen/movieapp/Model/Movie.java
@@ -9,6 +9,14 @@ import com.google.gson.annotations.SerializedName;
 public class Movie {
     @SerializedName("poster_path")
     private String posterPath;
+    @SerializedName("vote_average")
+    private String voteAverage;
+    @SerializedName("overview")
+    private String overview;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("original_title")
+    private String title;
 
     public Movie (String url){
         posterPath = url;
@@ -18,6 +26,19 @@ public class Movie {
         return posterPath;
     }
 
+    public String getVoteAverage() {
+        return voteAverage;
+    }
 
+    public String getOverview() {
+        return overview;
+    }
 
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
